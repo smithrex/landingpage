e0dd811cfde9e253630eb1ce4fae0ca20e82a335
Rodrigo Herrera <90879379+smithrex@users.noreply.github.com>
2021-10-06 02:02:12 +0000

    Mejorando formularios

--- app/controllers/proyectos_controller.rb
+++ app/controllers/proyectos_controller.rb
@@ -1,6 +1,7 @@
 class ProyectosController < ApplicationController
+  before_action :authenticate_user! 
   before_action :set_proyecto, only: [:show, :edit, :update, :destroy]
-
+  
   # GET /proyectos
   # GET /proyectos.json
   def index
