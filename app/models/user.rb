3899921057eea998f3a99d95f9224d96ef47f380
Rodrigo Herrera <90879379+smithrex@users.noreply.github.com>
2021-10-27 02:06:55 +0000

    relacion usuario cliente

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,19 @@
 class User < ApplicationRecord
+  after_save :enlazar_a_cliente 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  devise :database_authenticatable, :registerable,:recoverable, :rememberable, :validatable
+
+  def enlazar_a_cliente
+    cl = Cliente.where(user_id: self.id).first
+    if cl.blank?
+      cl = Cliente.new
+      cl.user_id = self.id
+      cl.nombres = "---"
+      cl.apell_pat = "---"
+      cl.apell_mat = "---"
+      cl.nif = "---"
+      cl.save
+    end
+  end
 end
